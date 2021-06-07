dc1b63fc26fa2028169a3d8ad9164b76cba5e508
BhargavJethwa <bhargavjethwa8@mail.com>
2021-06-07 12:21:44 +0530

    7th June

--- logistics/models.py
+++ logistics/models.py
@@ -36,7 +36,12 @@ class Vehicle(models.Model):
     Notifications = models.BooleanField(verbose_name="Notifications Needed", default=True)
     Insurance_expired = models.BooleanField(default=False)
     PUC_expired = models.BooleanField(default=False)
-    Available = models.BooleanField(default=False)
+    Available = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if(self.Other):
+            self.Other = self.Other.upper()
+        super(Vehicle, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.RC_number
